a9543dee36dfb0e5c9a77f59a8b0992b03a445ae
Carl Räfting <carl.rafting@gmail.com>
2023-03-09 23:46:45 +0100

    Update vanillapod.js

--- vanillapod.js
+++ vanillapod.js
@@ -1,2 +1,24 @@
 export { version } from './package.json';
-export * from './src/index.js';
+import mount from './src/mount';
+import debug from './src/debug';
+import setElementProperties from './src/properties';
+import setElementAttributes from './src/attributes';
+import setElementTextContent from './src/text';
+import setElementEventHandlers from './src/events';
+import createDocumentFragment from './src/fragment';
+
+export * from './src/dom';
+export * from './src/memo';
+export * from './src/state';
+export * from './src/error';
+export * from './src/element';
+export * from './src/hooks';
+export {
+    mount,
+    debug,
+    setElementProperties,
+    setElementAttributes,
+    setElementTextContent,
+    setElementEventHandlers,
+    createDocumentFragment,
+};
