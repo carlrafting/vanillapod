4a4c8d789444d4beb9e70e5ae35def1c01a55728
Carl Räfting <carl.rafting@gmail.com>
2023-03-10 15:06:40 +0100

    signals wip...

--- src/state.js
+++ src/state.js
@@ -1,4 +1,4 @@
-import { createError } from './error';
+import { createError } from './error.js';
 
 export function ministore(initialValue) {
     let value = initialValue;
@@ -6,15 +6,12 @@ export function ministore(initialValue) {
 
     function notifyListeners() {
         if (listeners.size === 0) {
-            createError('No listeners registered');
+            createError('No listeners registered!');
         }
         listeners.forEach((listener) => listener(value));
     }
 
     function read() {
-        const running = [...listeners][listeners.size - 1];
-        console.log({ running });
-        /* running && */ subscribe(running);
         return value;
     }
 
@@ -39,11 +36,77 @@ export const useStore = ministore;
 export const makeStore = ministore;
 export const createStore = ministore;
 
+/* function subscribe(running, listeners) {
+    listeners.add(running);
+    running.dependencies.add(running);
+} */
+
+const context = [];
+
 export function signal(value) {
     const s = store(value);
-    return [s.read, s.dispatch, s.subscribe];
+    const listeners = new Set();
+    const subscribe = (running) => {
+        listeners.add(running);
+        running.dependencies.add(running);
+    };
+    const read = () => {
+        const running = [...context][context.length - 1];
+        // console.log({ running });
+        if (running) subscribe(running);
+        return s.read();
+    };
+    const notify = () => {
+        for (const l of [...listeners]) {
+            l.execute();
+        }
+    };
+    const write = (nextValue) => {
+        s.dispatch(nextValue);
+        notify();
+    };
+    return [read, write];
 }
 
 export const createSignal = signal;
 export const useSignal = signal;
 export const makeSignal = signal;
+
+function cleanup(running) {
+    for (const dep of running.dependencies) {
+        console.log({ dep });
+        dep.delete(running);
+    }
+    running.dependencies.clear();
+}
+
+export function effect(fn) {
+    const running = {
+        execute() {
+            // 1. cleanup
+            cleanup(running);
+            // 2. push effect to context
+            context.push(running);
+            try {
+                // 3. execute callback fn
+                fn();
+            } catch (error) {
+                console.error(error);
+            } finally {
+                // 4. pop the last element from the context array
+                context.pop();
+            }
+        },
+        dependencies: new Set(),
+    };
+    running.execute();
+}
+
+export const createEffect = effect;
+export const makeEffect = effect;
+export const useEffect = effect;
+
+/* const [count, setCount] = signal(0);
+createEffect(() => console.log(count()));
+setCount(count() + 1);
+setCount(count() * 3); */
