d91a842a46c30d3c481f0ef0e8a432c80f8e8088
Carl Räfting <carl.rafting@gmail.com>
2023-03-14 12:55:42 +0100

    Update debug.js
    potential new debug api

--- src/debug.js
+++ src/debug.js
@@ -10,5 +10,31 @@ export default function debug(value = false) {
         return VANILLAPOD_DEBUG;
     }
 
-    return (VANILLAPOD_DEBUG = value);
+    if (typeof value === 'boolean') VANILLAPOD_DEBUG = value;
+}
+
+function createDebug(name = '') {
+    return (method = '') => ({
+        enable() {
+            VANILLAPOD_DEBUG = true;
+        },
+        disable() {
+            VANILLAPOD_DEBUG = false;
+        },
+        log(...output) {
+            console.log(new Date().toISOString(), name, method, ...output);
+        },
+    });
+}
+
+{
+    const debug = createDebug('vanillapod');
+    debug('mymethod').log('example');
+    const results = performance.measure('perf', () => {
+        const arr = [];
+        for (let i = 0; i < 1000; i += 1) {
+            arr.push(i);
+        }
+    });
+    debug('perf').log(results);
 }
