22aaf773c9b405f2b8dd75e1d796e5362f6b2b6d
Carl Räfting <carl.rafting@gmail.com>
2023-03-14 12:57:48 +0100

    Update state.js
    refactor methods

--- src/state.js
+++ src/state.js
@@ -1,34 +1,99 @@
+import debug from './debug.js';
 import { createError } from './error.js';
+import { createArray, checkType } from './utils.js';
 
+/*
+
+Usage:
+===========================================
+
+const count = store(0);
+
+count.subscribe(function logCount() {
+    console.log(count.read());
+});
+count.dispatch(count.read() + 1);
+count.dispatch(count.read() + 3);
+
+const message = store('');
+effect(function logMessage() {
+    console.log(message.read());
+});
+message.dispatch('...');
+message.dispatch('Hello World!');
+
+const [counter, setCounter] = createSignal(0);
+createEffect(() => {
+    console.log(counter())
+});
+setCounter(counter() + 3)
+setTimeout(() => setCounter(counter() * 6), 1000);
+
+*/
+
+console.time('state');
+
+const effects = createArray([]);
+
+debug('foo');
+console.log('debug', debug());
+
+function notify(listeners, value) {
+    console.time('notify');
+    if (listeners.size === 0) {
+        createError('state: No listeners registered!');
+    }
+    for (const listener of listeners) {
+        // console.log('notifyListeners', listener);
+        if (listener && checkType(listener) === 'function') listener(value);
+    }
+    console.timeEnd('notify');
+}
+
+function subscribe(listeners, fn) {
+    console.time('subscribe');
+    listeners.add(fn);
+    console.timeEnd('subscribe');
+    return function unsubscribe() {
+        listeners.delete(fn);
+    };
+}
+
+const unsubscribes = new Set();
+
+function scheduleEffect(listeners) {
+    console.time('scheduleEffect');
+    const effect = effects.last();
+    // effect && console.log('runListenerEffect', { effect });
+    if (effect) {
+        const unsubscribe = subscribe(listeners, effect);
+        unsubscribes.add(unsubscribe);
+        // console.log('unsubscribeFns', { unsubscribeFns });
+    }
+    console.timeEnd('scheduleEffect');
+}
+
+// a ministore method with automatic subscription and effect scheduling
 export function ministore(initialValue) {
     let value = initialValue;
     const listeners = new Set();
 
-    function notifyListeners() {
-        if (listeners.size === 0) {
-            createError('No listeners registered!');
-        }
-        listeners.forEach((listener) => listener(value));
-    }
-
     function read() {
+        scheduleEffect(listeners);
+        // console.log('read', { fx: effects.entries(), listeners });
         return value;
     }
 
     function dispatch(action) {
-        value = typeof action === 'function' ? action(value) : action;
-        notifyListeners();
+        value = checkType(action) === 'function' ? action(value) : action;
+        notify(listeners, value);
     }
 
-    function subscribe(listener) {
-        listeners.add(listener);
-
-        return function unsubscribe() {
-            listeners.delete(listener);
-        };
-    }
-
-    return { read, dispatch, subscribe };
+    return {
+        read,
+        dispatch,
+        subscribe: (fn) => effect(fn),
+    };
 }
 
 export const store = ministore;
@@ -36,76 +101,37 @@ export const useStore = ministore;
 export const makeStore = ministore;
 export const createStore = ministore;
 
-/* function subscribe(running, listeners) {
-    listeners.add(running);
-    running.dependencies.add(running);
-} */
-
-const context = [];
-
-export function signal(value) {
-    const listeners = new Set();
-    const subscribe = (running) => {
-        listeners.add(running);
-        running.dependencies.add(running);
-    };
-    const read = () => {
-        const running = context[context.length - 1];
-        // console.log({ running });
-        if (running) subscribe(running);
-        return value;
-    };
-    const notify = () => {
-        for (const l of [...listeners]) {
-            l.execute();
+function effect(fn) {
+    // console.log('createListenerEffect:', effects.entries());
+    console.time('effect');
+    const run = () => {
+        effects.clear();
+        effects.add(fn);
+        console.log({ effects: effects.entries() });
+        try {
+            fn();
+        } catch (error) {
+            console.log(error);
+        } finally {
+            effects.entries().pop();
         }
     };
-    const write = (nextValue) => {
-        value = nextValue;
-        notify();
-    };
-    return [read, write];
-}
-
-export const createSignal = signal;
-export const useSignal = signal;
-export const makeSignal = signal;
-
-function cleanup(running) {
-    for (const dep of running.dependencies) {
-        console.log({ dep });
-        dep.dependencies.delete(running);
-    }
-    running.dependencies.clear();
-}
-
-export function effect(fn) {
-    const running = {
-        execute() {
-            // 1. cleanup
-            cleanup(running);
-            // 2. push effect to context
-            context.push(running);
-            try {
-                // 3. execute callback fn
-                fn();
-            } catch (error) {
-                console.error(error);
-            } finally {
-                // 4. pop the last element from the context array
-                context.pop();
-            }
-        },
-        dependencies: new Set(),
-    };
-    running.execute();
+    run();
+    console.timeEnd('effect');
 }
 
 export const createEffect = effect;
 export const makeEffect = effect;
 export const useEffect = effect;
 
-/* const [count, setCount] = signal(0);
-createEffect(() => console.log(count()));
-setCount(count() + 1);
-setCount(count() * 3); */
+// a thin wrapper around store method with an api similar to solid.js
+export function signal(value) {
+    const s = store(value);
+    return [s.read, s.dispatch];
+}
+
+export const createSignal = signal;
+export const useSignal = signal;
+export const makeSignal = signal;
+
+console.timeEnd('state');
