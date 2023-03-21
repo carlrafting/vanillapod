c862844fa2ea306343777c36f3eb1ae08cbbeb6e
Carl Räfting <carl.rafting@gmail.com>
2023-03-21 15:21:31 +0100

    Update vanillapod.js
    export legacy api as well, remove in later version

--- vanillapod.js
+++ vanillapod.js
@@ -1,24 +1,18 @@
 export { version } from './package.json';
-/* import mount from './src/mount';
-import setElementProperties from './src/properties';
-import setElementAttributes from './src/attributes';
-import setElementTextContent from './src/text';
-import setElementEventHandlers from './src/events';
-import createDocumentFragment from './src/fragment';
- */
+
+// legacy
+export { default as mount } from './src/mount';
+export { default as setElementProperties } from './src/properties';
+export { default as setElementAttributes } from './src/attributes';
+export { default as setElementTextContent } from './src/text';
+export { default as setElementEventHandlers } from './src/events';
+export { default as createDocumentFragment } from './src/fragment';
+
+// new api exports
 export { default as debug } from './src/debug';
 export * from './src/dom';
 export * from './src/memo';
 export * from './src/state';
 export * from './src/error';
-// export * from './src/element';
-// export * from './src/hooks';
-/* export {
-    mount,
-    setElementProperties,
-    setElementAttributes,
-    setElementTextContent,
-    setElementEventHandlers,
-    createDocumentFragment,
-};
- */
+export * from './src/element';
+export * from './src/hooks';
