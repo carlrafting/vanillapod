bc23326f45b65e3cd5591f2ea9a8557fefa02b2f
Carl Räfting <121248+carlrafting@users.noreply.github.com>
2026-04-24 19:55:01 +0200

    clean slate

--- examples/todo/readme.md
+++
@@ -1,3 +0,0 @@
-# vanillapod todo example
-
-[Check out example app on netlify](https://dazzling-benz-6bc339.netlify.app/)
--- sandbox/hyper/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createMetaElement, ensureMetaElementExists } from "./hyper.js";
-
-document.head.append(createMetaElement());
-
-if (ensureMetaElementExists()) {
-  alert("hooray!");
-}
