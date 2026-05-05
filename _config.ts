34120a4f37d886234dca3bc01fbf250bb16f2cb1
Carl Räfting <mail@carlrafting.com>
2026-05-05 15:10:15 +0200

    add escape filter
    Signed-off-by: Carl Räfting <mail@carlrafting.com>

--- _config.ts
+++ _config.ts
@@ -1,11 +1,14 @@
 import lume from "lume/mod.ts";
+import prismPlugin from "lume/plugins/prism.ts";
+import { escape } from "@std/html";
 
 const site = lume({
   src: "docs",
 });
-
+site.use(prismPlugin());
 site.remote("assets/global.css", "assets/global.css");
 site.remote("logo.small.avif", "assets/logo.small.avif");
 site.copy([".css", ".js", ".png", ".avif"]);
+site.filter("escape", (input) => escape(input));
 
 export default site;
